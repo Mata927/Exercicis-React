005aa74ee5d056d12ced22ec7f44fc31db0aec78
Pau Mata Gómez <pmata279@gmail.com>
2022-02-16 19:45:11 +0100

    nuevo1

--- src/accordeo/src/App.js
+++ src/accordeo/src/App.js
@@ -1,49 +1,36 @@
 
+import Titol from './Titol.js';
 import './App.css';
 
 export default function App() {
  
-  function Titol({titols}) {
-    return (
-      <>
-          <ul>
-          const recorreArray = (titols) => {
-        for(let i=0; i<=llibreria.length-1; i++){
-      console.log(llibreria[i]);
-  }
-}
-          </ul>
-      </>
-    );
-  
-  const llibreria=[{
-    "title": "Titol 1",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a consequat nibh. Mauris suscipit arcu at fermentum convallis. Pellentesque consectetur mi in felis maximus posuere."
+  const acordeo=[ {
+    id: 1,title: "Titol 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a consequat nibh. Mauris suscipit arcu at fermentum convallis. Pellentesque consectetur mi in felis maximus posuere."
   },
   {
-    "title": "Titol 2",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie tellus a maximus tempus. Duis vel leo iaculis, porttitor erat et, posuere erat. Ut blandit."
+    id: 2, title : "Titol 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie tellus a maximus tempus. Duis vel leo iaculis, porttitor erat et, posuere erat. Ut blandit."
   },
   {
-    "title": "Titol 3",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, nibh imperdiet tempus pharetra, arcu risus aliquet arcu, a auctor ex lacus efficitur purus. Morbi."
+    id: 3, title: "Titol 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia, nibh imperdiet tempus pharetra, arcu risus aliquet arcu, a auctor ex lacus efficitur purus. Morbi."
   },
   {
-    "title": "Titol 4",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lobortis nibh, eu molestie est placerat non. Donec ornare nisl erat, non imperdiet elit porta."
+    id : 4,title: "Titol 4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lobortis nibh, eu molestie est placerat non. Donec ornare nisl erat, non imperdiet elit porta."
   },
   {
-  "title": "Titol 5",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lobortis nibh, eu molestie est placerat non. Donec ornare nisl erat, non imperdiet elit porta."
+  id: 5,title: "Titol 5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat lobortis nibh, eu molestie est placerat non. Donec ornare nisl erat, non imperdiet elit porta."
   }
 ];
 
-
   return (
     <div className="App">
-      <Libro titols={llibreria}/> 
+      <Titol acordeo={acordeo}/> 
       </div>
   );
-}
 
+}
 
--- src/accordeo/src/Titol.js
+++ src/accordeo/src/Titol.js
@@ -1,11 +1,32 @@
-export default function Titol({titols}) {
-    return (
-      <>
-          <ul>
-            {titols.map((n) => (
-              <li key={n}>{n}</li>
-            ))}
-          </ul>
-      </>
-    );
+import { useState } from "react";
+import './App.js';
+import './App.css';
+export default function Titol(props) {
+
+  const [selected,setSelected]=useState(null);
+
+  const toggle = (i) => {
+    if(selected === i){
+      return setSelected(null)
+    }
+    setSelected(i);
   }
+    return (
+      <div className="wrapper">
+      <div className='accordio'>
+        {props.acordeo.map((n,i) => (
+          <div className='objecto'>
+             <div className='titulo' onClick={ () => toggle(i)}>
+              <h1>{n.title}</h1>
+               <span>{selected === i ? '-' : '+'}</span>
+             </div>
+                <div className='contenido'>{n.description}</div>            
+                </div>
+            ))}
+            </div>
+     </div>
+    );
+
+}
+  
+    
